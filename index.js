4b275e96c0549e95ba9c2cf21d2688a21d168084
Romeo Lager <rlager@deloitte.com>
2024-03-13 13:20:11 -0700

    manager class

--- index.js
+++ index.js
@@ -4,11 +4,26 @@ const { SalesPerson } = require("./classes/SalesPerson")
 const { SoftwareEngineer } = require("./classes/SoftwareEngineer")
 
 // Run and debug your code here
+const Employees = require('./Employees');
 
+class Manager extends Employees {
+  #employeesManaged;
 
+  constructor(name, position, salary, department, employeesManaged = []) {
+    super(name, position, salary);
+    this.department = department;
+    this.#employeesManaged = employeesManaged;
+  }
+  getEmployeesManaged() {
+    return this.#employeesManaged;
+  }
 
+  setEmployeesManaged(employee) {
+    this.#employeesManaged.push(employee);
+  }
+}
 
-
+module.exports = Manager;
 
 module.exports = {
     Employees,
